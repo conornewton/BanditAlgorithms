80e027385c63cdc0413ecea65a85f70ec38a6606
Conor Newton <conornewton@gmail.com>
2021-03-07 16:10:59 +0000

    simulation rewrite

--- src/simulations.py
+++ src/simulations.py
@@ -2,52 +2,24 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-from arms import BenoulliArms
+from arms import param_arms
 from gie import GosInE
+from simulation_util import *
 
 t = 10000
-# delta = 0.4 # Difference between first and second largest mean rewards
+arms = param_arms(0.2, 0.7, 0.5, 4)
 
-iters = 1
+out_ucb = GosInE(2, arms, node_type = "UCB", eps = 1, alpha = 1)
+out_ucb.play(t, range(t))
+average_regret_ucb = average_regret(out_ucb.nodes, 0.7)
 
-average_regret_kl = np.zeros(t)
-average_regret_ucb = np.zeros(t)
+out_klucb = GosInE(2, arms, node_type = "KL-UCB", eps = 1, alpha = 1)
+out_klucb.play(t, range(t))
+average_regret_klucb = average_regret(out_klucb.nodes, 0.7)
 
-## When KLUCB get reward of 0, it never tries that arm again
-for i in range(iters):
-
-    arms = BenoulliArms([0.7, 0.5, 0.5, 0.5])
-    max_mean = max(arms.means)
-    unif = [[np.random.uniform() for _ in range(t)],
-            [np.random.uniform() for _ in range(t)],
-            [np.random.uniform() for _ in range(t)],
-            [np.random.uniform() for _ in range(t)]]
-
-    # print("UCB results")
-    ucb = GosInE(2, arms, node_type="UCB")
-    ucb.play_unif(t, range(t), unif)
-
-    average_regret_ucb = np.add(average_regret_ucb, np.true_divide(np.add(ucb.nodes[0].regret(max_mean), ucb.nodes[1].regret(max_mean)), 2))
-    # for node in ucb.nodes:
-    #     print(max(ucb.arms.means) * t- sum(node.rewards))
-    #     print(node.times_played)
-
-    # print("KLUCB results")
-    klucb = GosInE(2, arms, node_type="KL_UCB")
-    klucb.play_unif(t, range(t), unif)
-
-    # for node in klucb.nodes:
-    #     print(max(klucb.arms.means) * t - sum(node.rewards))
-    #     print(node.times_played)
-    #     print(node.empirical_means)
-
-    average_regret_kl = np.add(average_regret_kl, np.true_divide(np.add(klucb.nodes[0].regret(max_mean), klucb.nodes[1].regret(max_mean)), 2))
-
-average_regret_kl = np.true_divide(average_regret_kl, iters)
-average_regret_ucb = np.true_divide(average_regret_ucb, iters)
 
 plt.plot(average_regret_ucb, 'r')
-plt.plot(average_regret_kl, 'g')
+plt.plot(average_regret_klucb, 'g')
 plt.xlabel("T")
 plt.ylabel("Regret")
 plt.show()
