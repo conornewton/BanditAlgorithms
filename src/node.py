88fac0cc2ed7b6a6a5ae90fa5e1bc4390ff79a4f
Conor Newton <conornewton@gmail.com>
2021-03-07 16:08:11 +0000

    feat: node cumalative regret

--- src/node.py
+++ src/node.py
@@ -13,6 +13,8 @@ class Node:
         self.history = [] # Previous arm pulls
         self.rewards = [] # Rewards from previous arm pulls
 
+        self.recommendations_recieved = []
+
         self.rewards_per_arm = [0] * len(self.arms)
         self.times_played = [0] * len(self.arms)# Times each arm has been played
         self.empirical_means = [0] * len(self.arms)
@@ -27,6 +29,8 @@ class Node:
         self.rewards_per_arm[arm_id] += reward
         self.times_played[arm_id] += 1
         self.empirical_means[arm_id] = self.rewards_per_arm[arm_id] / self.times_played[arm_id]
+
+        self.recommendations_recieved.append(arm_id)
 
     def give_recommendation(self):
         return self.arms[self.times_played_phase.index(max(self.times_played_phase))]
@@ -43,6 +47,16 @@ class Node:
         self.phase += 1
         self.times_played_phase = [0] * len(self.arms)
 
-    def regret(self):
-        pass
+    def cum_reward(self):
+        cum = self.rewards
+        for i in range(1, len(cum)):
+            cum[i] += cum [i-1]
+
+        return cum
+
+    def regret(self, mean):
+        cum = self.cum_reward()
+        for i in range(len(cum)):
+            cum[i] = i * mean - cum[i]
+        return cum
 
